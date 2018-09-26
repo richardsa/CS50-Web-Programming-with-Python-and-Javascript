b9810888f9e779522df817fc797705e8f6e48d27
Richard Saldivar <>
2018-09-26 16:45:57 +0000

    work on chat function

--- project2/application.py
+++ project2/application.py
@@ -8,7 +8,25 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+channels = [{"channelname": "Default", "chats": [{"username": "richie", "message": "hi"}]}, {"channelname": "Main", "chats": [{"username": "bob", "message": "yo"}]}]
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", channels=channels)
+
+@app.route("/channel/<string:channelName>")
+def channel(channelName):
+    chatChannel = [item for item in channels if item["channelname"] == channelName]
+    return render_template("channel.html", chatChannel=chatChannel)
+
+@socketio.on("submit chat")
+def vote(data):
+    chatText = data["chatText"]
+    channel = data["channel"]
+    displayName = data["displayName"]
+    chatChannel = [item for item in channels if item["channelname"] == channel]
+    print(chatChannel[0])
+    print(chatChannel[0]['chats'])
+    chatChannel[0]['chats'].append({"username": displayName, "message": chatText})
+    emit("display chats", {"chatText": chatText, "displayName": displayName}, broadcast=True)
--- project2/static/main.js
+++ project2/static/main.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
     if (!localStorage.getItem('displayname'))
         $('#login-modal').modal('show');
 
+    // Connect to websocket
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
 
     const displayNameForm = document.getElementById('display-name-form');
     /* listen for form submit */
@@ -13,6 +16,26 @@ document.addEventListener('DOMContentLoaded', () => {
          $('#login-modal').modal('hide');
          $('#display-name').val('');
          localStorage.setItem('displayname', displayName);
+    });
+
+    socket.on('connect', () => {
+        const chatForm = document.getElementById('chatForm');
+        const channel = "Default"
+        const displayName = localStorage.getItem('displayname');
+        $(chatForm ).on('submit', function(e){
+            e.preventDefault();
+            const chatText = $('#chat-text').val();
+            socket.emit('submit chat', {'chatText': chatText, 'channel': channel, 'displayName': displayName});
+            $('#chat-text').val('');
+        });
+    });
+
+    // When a new vote is announced, add to the unordered list
+    socket.on('display chats', data => {
+        const li = document.createElement('li');
+        li.innerHTML = `username: ${data.displayName} and chat: ${data.chatText}`;
+        document.querySelector('#chats').append(li);
+
     });
 
 
